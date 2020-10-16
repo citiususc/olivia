9306acf1e24b21d2998f8d1bfe987f56120cec4f
deuxbot <deuxbot@gmail.com>
2020-10-16 13:13:12 +0200

    Change Travis JDK

--- upgrade-jogamp.sh
+++ upgrade-jogamp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Download the new JogAmp libs
-url_path=v2.4.0-rc-20200307
+url_path=v2.4.0-rc-20200307 # Change this for a newer version when available (https://jogamp.org/deployment/)
 mkdir newlib
 cd newlib
 jars=( 
@@ -24,15 +24,13 @@ jars=(
 	jogl-all-natives-windows-amd64.jar
 	jogl-all-natives-windows-i586.jar
 )
-for i in "${jars[@]}"; do
-    wget https://jogamp.org/deployment/${url_path=}/jar/$i  
-done
+for i in "${jars[@]}"; do wget https://jogamp.org/deployment/${url_path=}/jar/$i; done
 # Overwrite the old libs
 v=2.3.2
 mv gluegen-rt.jar gluegen-rt-$v.jar
-for jar in gluegen-rt-* ; do mv $jar ${jar//t-n/t-$v-n} ; done
+for jar in gluegen-rt-*; do mv $jar ${jar//t-n/t-$v-n}; done
 mv jogl-all.jar jogl-all-$v.jar
-for jar in jogl-all-* ; do mv $jar ${jar//l-n/l-$v-n} ; done
+for jar in jogl-all-*; do mv $jar ${jar//l-n/l-$v-n}; done
 cp * ../target/lib
 cd ..
 rm -rf newlib
